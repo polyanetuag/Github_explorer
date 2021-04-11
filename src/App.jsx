703529b29cb6cc5bacefd963d37feef86ae870cd
Polyane Tuag <pollyportugues@hotmail.com>
2021-04-11 18:37:58 -0300

    Estado do componente - useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
+import { Counter } from './components/Counter'
 import { Repositorylist } from './components/RepositoryList'
 import './styles/global.scss'
 
 export function App() {
-  return <Repositorylist />
+  return (
+    <>
+    <Repositorylist />
+    <Counter />
+    </>
+  )
 }
